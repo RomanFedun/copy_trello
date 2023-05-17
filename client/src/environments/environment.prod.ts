d95014ab61ecdf897480296609414327b0ab3f00
Roman Fedun <fero5uin@gmail.com>
2023-05-17 15:50:09 +0300

    add build: tsc to json

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:4001/api',
-    socketUrl: 'http://localhost:4001'
+    apiUrl: 'http://meancopytrello.click/api',
+    socketUrl: 'http://meancopytrello.click'
 };
