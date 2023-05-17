cdc6065207c5ee015091c7a61c9b1558abe85079
Roman Fedun <fero5uin@gmail.com>
2023-05-17 23:34:15 +0300

    done

--- client/src/environments/environments.prod.ts
+++ client/src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://meancopytrello.click/api',
-    socketUrl: 'https://meancopytrello.click'
+    apiUrl: 'https://eltrello.click/api',
+    socketUrl: 'https://eltrello.click'
 };
