78489c51532b58e7ed40d53372294859e07c1880
Roman Fedun <fero5uin@gmail.com>
2023-05-17 23:24:24 +0300

    done

--- client/src/environments/environments.ts
+++ client/src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://meancopytrello.click/api',
-    socketUrl: 'http://meancopytrello.click'
+    apiUrl: 'http://localhost:4001/api',
+    socketUrl: 'http://localhost:4001'
 };
