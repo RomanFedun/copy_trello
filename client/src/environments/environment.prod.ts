126716efa723d6aa2097a9dc106fd2a6aad1ddd7
Roman Fedun <fero5uin@gmail.com>
2023-05-18 15:30:02 +0300

    well

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:4001/api',
-    socketUrl: 'http://localhost:4001'
+    apiUrl: 'http://localhost:80/api',
+    socketUrl: 'http://localhost:80'
 };
