d4bad96074de3e8169eefd069b40fe3650e253a4
Roman Fedun <49636416+RomanFedun@users.noreply.github.com>
2023-05-19 20:15:46 +0300

    Delete server/dist directory

--- server/dist/helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getErrorMessage = void 0;
-const mongoose_1 = require("mongoose");
-const getErrorMessage = (err) => {
-    return err instanceof mongoose_1.Error ? err.message : String(err);
-};
-exports.getErrorMessage = getErrorMessage;
-//# sourceMappingURL=helpers.js.map
